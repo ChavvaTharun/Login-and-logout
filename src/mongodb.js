0081ea8f58ea8e0b3d3b638422c9b73fbd1b73c2
chavvatharun <127178096+ChavvaTharun@users.noreply.github.com>
2024-05-05 23:24:53 +0530

    Initial commit

---
+++ src/mongodb.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/login")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('login',logInSchema)
+
+module.exports=LogInCollection
